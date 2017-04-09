16bb854db421b196175be6a0947ced479064e8f6
Shashank <bhavshashank@gmail.com>
2017-04-09 14:04:27 +0530

    Adds new way to insert data
    Adds new way to inser data using the sprintf and %s and implode function to insert the data using the parameters passed through it.

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -1,5 +1,4 @@
 <?php 
-	
 	class QueryBuilder {
 		protected $pdo;
 		public function __construct($pdo) {
@@ -9,5 +8,19 @@
 			$statement = $this->pdo->prepare("SELECT * FROM {$table}");
 			$statement->execute();
 			return $statement->fetchAll(PDO::FETCH_CLASS);
+		}
+		public function insert($table, $parameters) {
+				$sql = sprintf(
+						'insert into %s (%s) values (%s)',
+						$table,
+						implode(', ', array_keys($parameters)),
+						':' . implode(', :', array_keys($parameters))
+					);
+				try {
+					$statement = $this->pdo->prepare($sql);
+					$statement->execute($parameters);
+				} catch (Exception $e) {
+					die('Whoops! Something went wrong.');
+				}
 		}
 	}
