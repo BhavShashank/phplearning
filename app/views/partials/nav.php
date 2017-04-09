4b69a0b9637811b6cd82058b45b5bdc169c54e6b
Shashank <bhavshashank@gmail.com>
2017-04-09 13:17:36 +0530

    Move Views/partials to app folder

--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -1,6 +1,6 @@
 <nav>
 		<li><a href="/">Home</a></li>
-		<li><a href="studio">Studio</a></li>
+		<li><a href="users">Users</a></li>
 		<li><a href="about">About</a></li>
 		<li><a href="contact">Contact</a></li>
 	</nav>
