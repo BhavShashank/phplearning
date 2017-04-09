29c628ff502e2885c1ca546b2369d4f042f2c3cf
Shashank <bhavshashank@gmail.com>
2017-04-09 14:07:39 +0530

    Add action GET and POST
    Add the  action GET and POST to check the user request type and respond accordingly. Creates the function get and post to handle the user request type, post function call the callAction to check if the method requested by the user exists or not and respond accordingly.

--- core/Router.php
+++ core/Router.php
@@ -1,18 +1,38 @@
 <?php 
+	namespace App\Core;
 	class Router {
-		protected $routes = [];
+		public $routes = [
+			'GET' => [],
+			'POST' => []
+		];
 		public static function load($file) {
 			$router = new static;
 			require $file;
 			return $router;
 		}
-		public function define($routes) {
-			$this->routes = $routes;
+		public function get($uri, $controller) {
+			$this->routes['GET'][$uri] = $controller;
 		}
-		public function direct($uri) {
-			if(array_key_exists($uri, $this->routes)) {
-				return $this->routes[$uri];
+		public function post($uri, $controller) {
+			$this->routes['POST'][$uri] = $controller;
+		}
+		public function direct($uri, $requestType) {
+			if(array_key_exists($uri, $this->routes[$requestType])) {
+				return $this->callAction(
+					...explode('@', $this->routes[$requestType][$uri])
+				);
 			}
 			throw new Exception('404 No file Found');
+		}
+		protected function callAction($controller, $action){
+			$controller = "App\\Controllers\\{$controller}";
+			$controller = new $controller;
+			if (!method_exists($controller, $action)) {
+				throw new Exception(
+					"Error Processing Request. No {$controller} assigneed for this {$action} action. "
+					);
+				
+			}
+			return $controller->$action();
 		}
 	}
